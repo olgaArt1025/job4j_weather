b5dd9cc57dbc3738fb415c3eca737d79e2a7a823
Olga_ <olga_art1025@mail.ru>
2022-07-15 23:50:28 +0500

    2. Spring boot Webflux [#328882]

---
+++ src/main/java/ru/job4j/weather/WeatherService.java
@@ -0,0 +1,45 @@
+package ru.job4j.weather;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class WeatherService {
+
+    private final Map<Integer, Weather> weathers = new ConcurrentHashMap<>();
+
+    {
+        weathers.put(1, new Weather(1, "Msc", 20));
+        weathers.put(2, new Weather(2, "SPb", 25));
+        weathers.put(3, new Weather(3, "Bryansk", 15));
+        weathers.put(4, new Weather(4, "Smolensk", 15));
+        weathers.put(5, new Weather(5, "Kiev", 15));
+        weathers.put(6, new Weather(6, "Minsk", 15));
+    }
+
+    public Mono<Weather> findById(Integer id) {
+        return Mono.justOrEmpty(weathers.get(id));
+    }
+
+    public Flux<Weather> all() {
+        return Flux.fromIterable(weathers.values());
+    }
+
+    public Mono<Weather> hottest() {
+       return Mono.justOrEmpty(weathers.values()
+               .stream()
+               .max(Comparator.comparingInt(Weather::getTemperature)));
+    }
+
+    public Flux<Weather> cityGreatThen(int temperature) {
+        return Flux.fromIterable(weathers.values())
+                .filter(weather -> weather.getTemperature() > temperature);
+    }
+}
